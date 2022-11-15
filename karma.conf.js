4eff40d62439cc4ddd3f4f19af1684c1dfc56715
M.Furkan ARDOĞAN <117767324+fmardogan@users.noreply.github.com>
2022-11-15 14:52:27 +0300

    [dev]: Travis

--- karma.conf.js
+++ karma.conf.js
@@ -33,6 +33,13 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
+
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
@@ -41,4 +48,6 @@ module.exports = function (config) {
     singleRun: false,
     restartOnFileChange: true
   });
+
+
 };
